9f129a3bb6d173ab1778bb7ccf7d5f0a043405d8
appuachu <87751449+appuachu@users.noreply.github.com>
2021-07-21 05:03:33 -0400

    Add files via upload

---
+++ clickjacking.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+echo "  
+
+ 
+       - - - -           -- - - - - - -         -            -       -              -
+     -         -         -                      -            -       -              -
+     -         -         -                      -            -       -              -
+     -         -         -                      -            -       -              -
+     -=========-         -                      -============-       -              -
+     -         -         -                      -            -       -              -
+     -         -         -                      -            -       -              -
+     -         -         -                      -            -       -              -
+     -         -         - - - - - - - -        -            -       - - - - - - - -
+
+
+
+echo "****************************************************"
+echo "Copyright of ACHU, 2021                             "
+echo "                                                    "
+echo "                                                    "
+echo  "***************************************************"
+
+
+"
+
+dom=$1
+read -p "Enter URL (including https OR http) :" dom
+url=$dom
+echo "
+<html>
+<head>
+<title>ClickJacking vulnerable</title>
+</head>
+<p>Check the website is vulnerable for clickjacking</p>
+<iframe src="$url" width="500" height="500"></iframe>
+<p>If the website is shown on the box then it is vulnerable for Clickjacking</p>
+</body>
+</html>
+
+">clickjacking.html
+
+firefox clickjacking.html
+
+
